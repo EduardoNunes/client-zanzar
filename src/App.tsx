7fe90d41bf87f81f26cafdc0e6bf7adb33de5339
Eduardo <eduardolagonunes@gmail.com>
2025-01-27 17:53:17 -0300

    FEAT: adicionando rotas protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ToastContainer } from "react-toastify";
+import Feed from "./pages/Feed";
+import Layout from "./components/Layout";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import CreatePost from "./pages/CreatePost";
 
 export default function App() {
+  const [isTokenLoaded, setIsTokenLoaded] = useState(false);
+  const [token, setToken] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const loadedToken = Cookies.get("access_token");
+    setToken(loadedToken);
+    setIsTokenLoaded(true);
+  }, []);
+
+  const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    if (!isTokenLoaded) {
+      return null;
+    }
+
+    if (!token) {
+      return <Navigate to="/login" replace />;
+    }
+
+    return <>{children}</>;
+  };
+
   return (
     <>
       <ToastContainer
@@ -23,6 +49,28 @@ export default function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+
+          {/* Rota protegida */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <Feed />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <CreatePost />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
 
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
