022326f68a58024594551ad83ab7ff50fa5147b8
Eduardo <eduardolagonunes@gmail.com>
2025-01-29 09:36:38 -0300

    FEAT: adicionado ape de comentários nas postagens

--- src/requests/feedRequests.ts
+++ src/requests/feedRequests.ts
@@ -53,3 +53,33 @@ export const handleLikeReq = async (postId: string, userId: string) => {
     throw new Error(errorMessage);
   }
 };
+
+export const handleCommentReq = async (
+  postId: string,
+  userId: string,
+  content: string
+) => {
+  const token = Cookies.get("access_token");
+  if (!token) {
+    toast.error("Token de acesso não encontrado.");
+    return;
+  }
+
+  try {
+    const response = await api.post(
+      "/posts/comments",
+      { postId, userId, content },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error: any) {
+    const errorMessage =
+      error.response?.data?.message || "Erro ao buscar feed.";
+    throw new Error(errorMessage);
+  }
+};
