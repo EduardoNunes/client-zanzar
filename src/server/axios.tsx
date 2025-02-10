ede65308612858247b8cce8df8bf1f4af74d3286
Eduardo <eduardolagonunes@gmail.com>
2025-02-10 15:06:48 -0300

    REFACTOR: tirando comentário

--- src/server/axios.tsx
+++ src/server/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: /* "http://localhost:3001" */ "https://server-zanzar.onrender.com",
+  baseURL: "https://server-zanzar.onrender.com",
   timeout: 100000,
   headers: { "Content-Type": "application/json" },
 });
