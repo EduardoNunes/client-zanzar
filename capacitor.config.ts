880f67dfc453f0f1d0145f1c681a349460470821
Eduardo <eduardolagonunes@gmail.com>
2025-02-10 14:31:14 -0300

    FEAT: habilitando o server no capacitor para abrir no app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.zanzar',
   appName: 'zanzar',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://server-zanzar.onrender.com',
+  },
 };
 
 export default config;
