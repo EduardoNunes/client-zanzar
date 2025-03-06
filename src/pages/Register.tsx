bbf97d199c6474f6da20935e09c28d64458b27da
Eduardo <eduardolagonunes@gmail.com>
2025-03-06 17:04:26 -0300

    FEAT: adicionados: visualizar senha, logo, traduzido página e confirmar senha

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,17 +1,21 @@
-import { UserPlus } from "lucide-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { registerUserReq } from "../requests/registerRequests";
 import { registerSchema } from "../validations/registerSchema";
+import LogoZanzar from "../assets/logo-zanzar-indigo-96.png";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function Register() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState<string[]>([]);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,9 +24,13 @@ export default function Register() {
 
     try {
       await registerSchema.validate(
-        { email, password, username },
+        { email, password, username, confirmPassword },
         { abortEarly: false }
       );
+
+      if (password !== confirmPassword) {
+        throw new Error("As senhas não coincidem.");
+      }
 
       await registerUserReq(email, password, username);
 
@@ -50,14 +58,22 @@ export default function Register() {
     setUsername(sanitizedValue);
   };
 
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-100 to-white flex items-center justify-center p-4">
       <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
-        <div className="flex items-center justify-center mb-8">
-          <UserPlus className="w-12 h-12 text-indigo-600" />
+        <div className="flex flex-col items-center justify-center mb-6">
+          <img src={LogoZanzar} alt="Zanzar Logo" className="w-24 h-24" />
         </div>
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-8">
-          Register
+          Cadastrar
         </h2>
         {errors.length > 0 && (
           <div className="bg-red-50 border-l-4 border-red-500 text-red-700 p-4 mb-6 rounded">
@@ -71,7 +87,7 @@ export default function Register() {
         <form onSubmit={handleRegister} className="space-y-6">
           <div>
             <label className="block text-gray-700 text-sm font-semibold mb-2">
-              Username
+              Nome de usuário
             </label>
             <input
               type="text"
@@ -83,7 +99,7 @@ export default function Register() {
           </div>
           <div>
             <label className="block text-gray-700 text-sm font-semibold mb-2">
-              Email
+              E-mail
             </label>
             <input
               type="email"
@@ -95,15 +111,45 @@ export default function Register() {
           </div>
           <div>
             <label className="block text-gray-700 text-sm font-semibold mb-2">
-              Password
+              Senha
             </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-transparent transition"
-              required
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-transparent transition"
+                required
+              />
+              <button
+                type="button"
+                onClick={toggleShowPassword}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-600"
+              >
+                {showPassword ? <EyeOff size={20}/> : <Eye size={20}/>}
+              </button>
+            </div>
+          </div>
+          <div>
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Confirmar Senha
+            </label>
+            <div className="relative">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:border-transparent transition"
+                required
+              />
+              <button
+                type="button"
+                onClick={toggleShowConfirmPassword}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-600"
+              >
+               {showConfirmPassword ? <EyeOff size={20}/> : <Eye size={20}/>}
+              </button>
+            </div>
           </div>
 
           <button
@@ -116,17 +162,17 @@ export default function Register() {
                 <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
               </div>
             ) : (
-              "Register"
+              "Cadastrar"
             )}
           </button>
         </form>
         <p className="mt-8 text-center text-gray-600">
-          Already have an account?{" "}
+          Você já está cadastrado?{" "}
           <Link
             to="/login"
             className="text-indigo-600 hover:text-indigo-800 font-semibold"
           >
-            Login
+            Acessar
           </Link>
         </p>
       </div>
