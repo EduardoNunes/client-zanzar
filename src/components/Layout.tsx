39af784c0b7faaa7f9da76de9c25314910c414b8
Eduardo <eduardolagonunes@gmail.com>
2025-06-11 16:05:08 -0300

    TESTE: tentando aparecer o menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -138,7 +138,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   ];
 
   return (
-    <div className="relative h-full bg-gray-100">
+    <div className="h-screen bg-gray-100">
       <nav className="bg-white shadow-sm sticky top-0 z-50">
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex justify-between items-center h-16">
@@ -298,7 +298,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </div>
       </nav>
-
+      
       <main className="max-w-5xl mx-auto px-4 py-8">
         <AdModal />
         {children}
