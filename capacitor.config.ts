520dcae792c7d0b98d02ea360fb647c712b69a2b
Eduardo <eduardolagonunes@gmail.com>
2025-08-19 11:41:42 -0300

    TESTE 8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,22 +1,26 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.example.zanzar",
-  appName: "Zanzar",
-  webDir: "dist",
   plugins: {
     Camera: {
-      webUseInput: true,
+      webUseInput: true, // Isso força o uso do input file no PWA
     },
     StatusBar: {
       overlaysWebView: false,
     },
+    cordova: {
+      preferences: {
+        StatusBarBackgroundColor: "#000000", // Status bar background color (black in this example)
+        NavigationBarBackgroundColor: "#000000", // Navigation bar background color (black in this example)
+      },
+    },
   },
+  appId: "com.example.zanzar",
+  appName: "Zanzar",
+  webDir: "dist",
   server: {
-    androidScheme: "https",
-    // ❌ Remover "url" em produção
-    // url: "https://zanzar.netlify.app",
-    cleartext: true,
+    url: "https://zanzar.netlify.app",
+    cleartext: true, // Se você estiver usando HTTP durante o desenvolvimento local, isso pode ser necessário
   },
 };
 
